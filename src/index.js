da46e26cd2215ba6a042911931b8106ffcb42839
SobolevYuri <yura.sobolev.91@gmail.com>
2018-02-23 02:27:12 +0300

    My 5th task (Currencies)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
 // PLEASE DON'T change function name
+var ParCoin={"H":50,"Q":25,"D":10,"N":5,"P":1};
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    let Coins=currency;
+    let Result={},current;
+if(Coins<=0)
+{
+    return Result;
 }
+ if(Coins>10000)
+{
+    return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+}
+
+for (let key in ParCoin){
+    if(Coins==0){
+        return Result;
+    }
+
+current=ParCoin[key];
+let curCoins=Math.floor(Coins/current);
+if(curCoins==0){
+    continue;
+}
+Result[key]=curCoins;
+Coins=Coins%current;
+}
+return Result;
+}
+
+
